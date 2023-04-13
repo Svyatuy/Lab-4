6089c42160706a82cce81541624b030b7bab3d7b
Svyatuy <112246649+Svyatuy@users.noreply.github.com>
2023-04-13 22:41:59 +0300

    Lab-4
    Lab-4

---
+++ Lab-4/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using Event_Throttler;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventThrottler eventThrottler = new EventThrottler();
+
+            System.EventHandler handler1 = (sender, e) =>
+            {
+                Console.WriteLine($"Handler 1 triggered at {DateTime.Now}");
+            };
+
+            System.EventHandler handler2 = (sender, e) =>
+            {
+                Console.WriteLine($"Handler 2 triggered at {DateTime.Now}");
+            };
+
+            eventThrottler.RegisterEventHandler("event1", handler1);
+            eventThrottler.RegisterEventHandler("event1", handler2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                eventThrottler.TriggerEvent("event1", EventArgs.Empty, 1000);
+                Thread.Sleep(500);
+            }
+
+            eventThrottler.UnregisterEventHandler("event1", handler2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                eventThrottler.TriggerEvent("event1", EventArgs.Empty, 1000);
+                Thread.Sleep(500);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
